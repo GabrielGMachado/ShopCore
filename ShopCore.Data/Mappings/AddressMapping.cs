1ccfffb65ee7e13a6234b54e11054992ede5c53a
GabrielGMachado <bielgaelzermachado@gmail.com>
2025-09-29 10:51:03 -0300

    Configure Mappings

---
+++ ShopCore.Data/Mappings/AddressMapping.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopCore.Domain.Models;
+
+namespace ShopCore.Data.Mappings
+{
+    public class AddressMapping : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            // Name Table
+            builder.ToTable("Address");
+
+            // Id
+            builder.HasKey(x => x.Id);
+
+            //Street
+            builder.Property(x => x.Street)
+                .IsRequired();
+
+            // Number
+            builder.Property(x => x.Number)
+                .IsRequired();
+
+            // Neighborhood
+            builder.Property(x => x.Neighborhood)
+                .IsRequired();
+
+            // Postal Code
+            builder.Property(x => x.PostalCode)
+                .IsRequired();
+
+            //City 
+            builder.Property(x => x.City)
+                .IsRequired();
+
+            // State
+            builder.Property(x => x.State)
+                .IsRequired();
+
+            // 1:N
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.Addresses)
+                .HasForeignKey(x => x.UserId);
+        }
+    }
+}
---
+++ ShopCore.Data/Mappings/CartMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopCore.Domain.Models;
+
+namespace ShopCore.Data.Mappings;
+
+public class CartMapping : IEntityTypeConfiguration<Cart>
+{
+    public void Configure(EntityTypeBuilder<Cart> builder)
+    {
+        builder.ToTable("Cart");
+
+        builder.HasKey(x => x.Id);
+
+        // 1:1
+        builder.HasOne(x => x.User)
+            .WithOne(x => x.Cart)
+            .HasForeignKey<Cart>(x => x.UserId); // Precisa do <Cart> para ele saber em qual Entity vai ta a chave estrangeira
+
+        // 1:N
+        builder.HasMany(x => x.CartItems)
+            .WithOne(x => x.Cart);
+    }
+}
